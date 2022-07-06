b7f6003ed33bbf1515b74e14bf4253efe0258d16
fastfabien56 <fastfabien56@gmail.com>
2022-07-06 19:32:45 +0000

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>
+        List Product
+      </main>
+    </div>
+  );
+}
+
+export default App;
